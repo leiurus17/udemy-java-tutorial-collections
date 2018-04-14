72eba54506a3b65bbebbc4c752f68a8efa3cf0d5
Marlon <martianworm17@gmail.com>
2018-04-14 16:34:05 +0800

    Linked Lists

--- src/com/kamotelabs/collections02/App.java
+++ src/com/kamotelabs/collections02/App.java
@@ -7,9 +7,23 @@ import java.util.List;
 public class App {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * ArrayList manage arrays internally
+		 * [0][1][2][3][4][5] ....
+		 * 
+		 */
 
 		// if you want to add or remove only at the end of the list, use an ArrayList
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		/*
+		 * LinkedList consists of elements where each element
+		 * has a reference to the previous and next element
+		 * [0] -> [1] -> [2] ....
+		 *     <-     <-
+		 * 
+		 */
 		
 		// if you want to add or remove anywhere on the list, use a LinkedList
 		LinkedList<Integer> linkedList = new LinkedList<Integer>();
