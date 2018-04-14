b1d73a6e6a4280ac122de69f7cd3ce10b647dc61
Marlon <martianworm17@gmail.com>
2018-04-15 01:22:48 +0800

    Sets

--- src/com/kamotelabs/collections05/App.java
+++ src/com/kamotelabs/collections05/App.java
@@ -1,8 +1,26 @@
 package com.kamotelabs.collections05;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		Set<String> set1 = new HashSet<String>();
+		
+		set1.add("Dog");
+		set1.add("Cat");
+		set1.add("Mouse");
+		set1.add("Snake");
+		set1.add("Bear");
+		
+		// adding duplicate items does nothing.
+		
+		set1.add("Mouse");
+		
+		System.out.println(set1);
+		
 
 	}
 
