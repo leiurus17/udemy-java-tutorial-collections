0d06fb6a2810ce4d56b8a6541aa8fa92129ba8c1
Marlon <martianworm17@gmail.com>
2018-04-14 16:59:52 +0800

    HashMap: Retrieving Objects via a Key

--- src/com/kamotelabs/collections03/App.java
+++ src/com/kamotelabs/collections03/App.java
@@ -1,9 +1,38 @@
 package com.kamotelabs.collections03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
 	public static void main(String[] args) {
 
+		/*
+		 * HashMap is not sorted
+		 * The order is not maintained
+		 * 
+		 */
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		
+		map.put(5, "Five");
+		map.put(8, "Eight");
+		map.put(6, "Six");
+		map.put(4, "Four");
+		map.put(2, "Two");
+		
+		// overwrite the previous map key-value pairs
+		// map.put(6, "Hello");
+		
+		String text = map.get(6);
+		
+		System.out.println(text);
+		
+		for(Map.Entry<Integer, String> entry : map.entrySet()) {
+			int key = entry.getKey();
+			String value = entry.getValue();
+			
+			System.out.println("Key is: " + key + " and value is: " + value);
+		}
 	}
 
 }
