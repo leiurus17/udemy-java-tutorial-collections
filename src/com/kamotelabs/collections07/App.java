ae0ab37d78e579f8141d9da86311d2bef4431ea4
Marlon <martianworm17@gmail.com>
2018-04-15 17:15:14 +0800

    Sorting Lists

--- src/com/kamotelabs/collections07/App.java
+++ src/com/kamotelabs/collections07/App.java
@@ -8,7 +8,7 @@ import java.util.List;
 class Person {
 	private int id;
 	private String name;
-	
+
 	public Person(int id, String name) {
 		this.id = id;
 		this.name = name;
@@ -29,52 +29,51 @@ class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
-	
-	
+
 }
 
 class StringLengthComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		
+
 		int len1 = s1.length();
 		int len2 = s2.length();
-		
-		if(len1 > len2) {
+
+		if (len1 > len2) {
 			return 1;
-		} else if(len1 < len2) {
+		} else if (len1 < len2) {
 			return -1;
 		}
-		
+
 		return 0;
 	}
-	
+
 }
 
 class AlphabeticalComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		
+
 		return s1.compareTo(s2);
 	}
-	
+
 }
 
 class ReverseAlphabeticalComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		
+
 		// negative sign
 		return -s1.compareTo(s2);
 	}
-	
+
 }
 
 public class App {
@@ -98,7 +97,7 @@ public class App {
 
 		// static method to sort list on string length
 		// Collections.sort(animals, new StringLengthComparator());
-		
+
 		Collections.sort(animals, new ReverseAlphabeticalComparator());
 
 		for (String animal : animals) {
@@ -122,39 +121,59 @@ public class App {
 			public int compare(Integer num1, Integer num2) {
 				return -num1.compareTo(num2);
 			}
-			
+
 		});
 
 		for (Integer number : numbers) {
 			System.out.println(number);
 		}
-		
+
 		List<Person> people = new ArrayList<Person>();
-		
+
 		people.add(new Person(1, "Joe"));
 		people.add(new Person(2, "Sue"));
 		people.add(new Person(3, "Bob"));
 		people.add(new Person(4, "Clare"));
-		
-		for(Person person: people) {
+
+		for (Person person : people) {
 			System.out.println(person);
 		}
-		
+
+		// sort in order of id
 		Collections.sort(people, new Comparator<Person>() {
 
 			@Override
 			public int compare(Person p1, Person p2) {
-				
-				if(p1.getId() > p2.getId()) {
+
+				if (p1.getId() > p2.getId()) {
 					return 1;
 				} else if (p1.getId() < p2.getId()) {
 					return -1;
 				}
-				
+
 				return 0;
 			}
-			
+
 		});
+		
+		for (Person person : people) {
+			System.out.println(person);
+		}
+
+		// sort in order of id
+		Collections.sort(people, new Comparator<Person>() {
+
+			@Override
+			public int compare(Person p1, Person p2) {
+
+				return p1.getName().compareTo(p2.getName());
+			}
+
+		});
+		
+		for (Person person : people) {
+			System.out.println(person);
+		}
 
 	}
 
