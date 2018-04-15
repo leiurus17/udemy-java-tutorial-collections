9a98b222098162a26539b7a3679d717e9eaa0f54
Marlon <martianworm17@gmail.com>
2018-04-15 16:30:41 +0800

    Using Custom Objects in Sets and as Keys in Maps

--- src/com/kamotelabs/collections06/App.java
+++ src/com/kamotelabs/collections06/App.java
@@ -1,13 +1,15 @@
 package com.kamotelabs.collections06;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		Map<String, Integer> map = new HashMap<String, Integer>();
+		Map<String, Integer> map = new LinkedHashMap<String, Integer>();
 		
 		map.put("one", 1);
 		map.put("two", 2);
@@ -17,6 +19,16 @@ public class App {
 		for(String key : map.keySet()) {
 			System.out.println(key + ": " + map.get(key));
 		}
+		
+		Set<String> set = new LinkedHashSet<String>();
+		
+		set.add("dog");
+		set.add("cat");
+		set.add("mouse");
+		set.add("cat");
+		
+		System.out.println(set);
+		
 	}
 
 }
