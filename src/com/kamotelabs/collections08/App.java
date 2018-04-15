5ccd9bf228e628c33149e610390fde78ddf2e72c
Marlon <martianworm17@gmail.com>
2018-04-15 18:56:05 +0800

    Natural Ordering

--- src/com/kamotelabs/collections08/App.java
+++ src/com/kamotelabs/collections08/App.java
@@ -2,11 +2,12 @@ package com.kamotelabs.collections08;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-class Person {
+class Person implements Comparable<Person> {
 	private String name;
 	
 	public Person(String name) {
@@ -16,6 +17,35 @@ class Person {
 	@Override
 	public String toString() {
 		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	public int compareTo(Person person) {
+		return name.compareTo(person.name);
 	}
 }
 
@@ -27,6 +57,8 @@ public class App {
 		
 		addElements(list);
 		addElements(set);
+		
+		Collections.sort(list);
 		
 		showElements(list);
 		System.out.println();
