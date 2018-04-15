fd0aea08066a687c253f565097c40a158e7b431e
Marlon <martianworm17@gmail.com>
2018-04-15 16:57:52 +0800

    Sorting Lists

--- src/com/kamotelabs/collections07/App.java
+++ src/com/kamotelabs/collections07/App.java
@@ -2,7 +2,27 @@ package com.kamotelabs.collections07;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class StringLengthComparator implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		
+		int len1 = s1.length();
+		int len2 = s2.length();
+		
+		if(len1 > len2) {
+			return 1;
+		} else if(len1 < len2) {
+			return -1;
+		}
+		
+		return 0;
+	}
+	
+}
 
 public class App {
 
