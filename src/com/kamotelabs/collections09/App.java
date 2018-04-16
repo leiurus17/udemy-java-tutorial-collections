0c5748168ed03a4c91312d84065c6b660cdfd837
Marlon <martianworm17@gmail.com>
2018-04-16 19:19:18 +0800

    Queues

--- src/com/kamotelabs/collections09/App.java
+++ src/com/kamotelabs/collections09/App.java
@@ -1,8 +1,31 @@
 package com.kamotelabs.collections09;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		// head and tail queue
+		// first in first out
+		
+		Queue<Integer> q1 = new ArrayBlockingQueue<Integer>(3);
+		
+		q1.add(10);
+		q1.add(20);
+		q1.add(30);
+		
+		
+		try {
+			q1.add(40);
+		} catch (IllegalStateException e) {
+			System.out.println("Tried to add to many items to the queue.");
+		}
+		
+		for(Integer value: q1) {
+			System.out.println("Queue value: " + value);
+		}
 
 	}
 
