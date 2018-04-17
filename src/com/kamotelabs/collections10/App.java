ba4fefdb1ca41ddd95af46ce184a6917f6847d54
Marlon <martianworm17@gmail.com>
2018-04-17 20:02:19 +0800

    Using Iterators

--- src/com/kamotelabs/collections10/App.java
+++ src/com/kamotelabs/collections10/App.java
@@ -1,8 +1,33 @@
 package com.kamotelabs.collections10;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class App {
 
 	public static void main(String[] args) {
+
+		LinkedList<String> animals = new LinkedList<String>();
+
+		animals.add("fox");
+		animals.add("cat");
+		animals.add("dog");
+		animals.add("rabbit");
+
+		// Before Java 5.
+
+		Iterator<String> it = animals.iterator();
+
+		while (it.hasNext()) {
+			String value = it.next();
+			System.out.println(value);
+		}
+
+		// Modern iteration, Java 5 and later
+
+		for (String animal : animals) {
+			System.out.println(animal);
+		}
 
 	}
 
