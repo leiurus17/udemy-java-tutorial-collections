11e610a68a3c218f586cac2e5c8b8bca421099c8
Marlon <martianworm17@gmail.com>
2018-04-18 20:45:53 +0800

    Complex Data Structures

--- src/com/kamotelabs/collections13/App.java
+++ src/com/kamotelabs/collections13/App.java
@@ -1,6 +1,16 @@
 package com.kamotelabs.collections13;
 
 public class App {
+	
+	public static String[] vehicles = {
+			"ambulance", "helicopter", "lifeboat"
+	};
+	
+	public static String[][] drivers = {
+			{"Fred", "Sue", "Pete"},
+			{"Sue", "Richard", "Bob", "Fred"},
+			{"Pete", "Mary", "Bob"},
+	};
 
 	public static void main(String[] args) {
 
